967f6ad8ff59a117b3851e0b8f110819cc334cc8
Sebastian Delmont <sd@notso.net>
2022-03-02 08:42:20 -0500

    `nx g @nrwl/js:library qson/cabrillo --publishable --importPath="@ham2k/qson/cabrillo" --js`
    Plus fixes to the generated `package.json`

--- apps/stats/src/app/app.jsx
+++ apps/stats/src/app/app.jsx
@@ -1,10 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
+import { qsonCabrillo } from '@ham2k/qson/cabrillo'
+
 export function App() {
   return (
     <>
       <h1>Ham2K</h1>
+      {qsonCabrillo()}
       <div />
     </>
   );
